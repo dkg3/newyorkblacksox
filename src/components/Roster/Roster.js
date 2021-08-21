0e427e99e8fe314815a4f1af718e899595ada00a
dkrauskopf-greene <David.Greene@mlb.com>
2021-08-21 01:16:01 -0400

    Roster Updates

--- src/components/Roster/Roster.js
+++ src/components/Roster/Roster.js
@@ -3,46 +3,59 @@ import PropTypes from 'prop-types';
 
 import Player from './Players/Players';
 
-let season = 'Season';
+// const roster = '2021 Roster';
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById('myDropdown').classList.toggle('show');
-}
+// function myFunction() {
+//   document.getElementById('myDropdown').classList.toggle('show');
+// }
 
-function pickSeason() {
-  season = '2019 Roster';
-}
+// function myDivFunction() {
+//   document.getElementById('myDropdown').classList.toggle('show');
+// }
+
+// function pickSeason(year) {
+//   const container = document.createElement('div');
+//   container.id = 'dropdownArrow';
+//   container.className = 'arrow down';
+//   document.getElementById('myDropdown').classList.toggle('show');
+//   document.getElementById('dropdownArrow').classList.toggle('up');
+//   document.getElementById('dropdownArrow').classList.toggle('down');
+//   document.getElementsByClassName('dropbtn')[0].innerHTML = year;
+//   document.getElementsByClassName('dropbtn')[0].append(container);
+//   roster = year;
+// }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = (event) => {
-  if (!event.target.matches('.dropbtn')) {
-    const dropdowns = document.getElementsByClassName('dropdown-content');
-    let i;
-    for (i = 0; i < dropdowns.length; i += 1) {
-      const openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-        document.getElementById('dropdownArrow').classList.toggle('up');
-        document.getElementById('dropdownArrow').classList.toggle('down');
-      }
-    }
-  } else {
-    document.getElementById('dropdownArrow').classList.toggle('up');
-    document.getElementById('dropdownArrow').classList.toggle('down');
-  }
-};
+// window.onclick = (event) => {
+//   if (!event.target.matches('.dropbtn')) {
+//     const dropdowns = document.getElementsByClassName('dropdown-content');
+//     let i;
+//     for (i = 0; i < dropdowns.length; i += 1) {
+//       const openDropdown = dropdowns[i];
+//       if (openDropdown.classList.contains('show')) {
+//         openDropdown.classList.remove('show');
+//         document.getElementById('dropdownArrow').classList.toggle('up');
+//         document.getElementById('dropdownArrow').classList.toggle('down');
+//       }
+//     }
+//   } else {
+//     document.getElementById('dropdownArrow').classList.toggle('up');
+//     document.getElementById('dropdownArrow').classList.toggle('down');
+//   }
+// };
 
 const Players = ({ data }) => (
   <div className="players">
     <div className="title">
       <h3>Players/Coaches</h3>
-      <button type="button" onClick={myFunction} className="dropbtn">{season} <i onClick={myFunction} id="dropdownArrow" className="arrow down" /></button>
-      <div id="myDropdown" className="dropdown-content">
-        <button type="button" id="2021Button" onClick={pickSeason} value="2021 Roster">2021 Roster</button>
-        <button type="button" id="2019Button" onClick={pickSeason} value="2019 Roster">2019 Roster</button>
-      </div>
+      {/* eslint-disable max-len */}
+      {/* <button type="button" onClick={myFunction} className="dropbtn">{roster} <div onClick={myDivFunction} id="dropdownArrow" className="arrow down" /></button> */}
+      {/* <div id="myDropdown" className="dropdown-content">
+        <button type="button" id="2021Button" onClick={() => pickSeason('2021 Roster ')} value="2021 Roster ">2021 Roster</button>
+        <button type="button" id="2019Button" onClick={() => pickSeason('2019 Roster ')} value="2019 Roster ">2019 Roster</button>
+      </div> */}
     </div>
     {data.map((player) => (
       <Player
--- src/pages/Roster.js
+++ src/pages/Roster.js
@@ -6,14 +6,9 @@ import Main from '../layouts/Main';
 import Players from '../components/Roster/Roster';
 
 import players2021 from '../data/roster/players_2021';
-import players2019 from '../data/roster/players_2019';
+// import players2019 from '../data/roster/players_2019';
 
-function checkYear() {
-  if (window.location.href.includes('2019')) {
-    return players2019;
-  }
-  return players2021;
-}
+const rosterData = players2021;
 
 const Roster = () => (
   <Main
@@ -26,8 +21,7 @@ const Roster = () => (
           <h2 data-testid="heading"><Link to="/roster">Roster</Link></h2>
         </div>
       </header>
-      <Players data={checkYear()} />
-
+      <Players data={rosterData} />
     </article>
   </Main>
 );
